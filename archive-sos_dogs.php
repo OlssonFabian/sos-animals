4a065fc20429bcc0b822ebeb61c02b479f5b2b78
Fabian Olsson <fabtasticdesign@live.se>
2019-05-04 18:02:29 +0200

    flexbox fix for dog archive

--- archive-sos_dogs.php
+++ archive-sos_dogs.php
@@ -16,9 +16,9 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="archive-wrapper">
+<div class="wrapper d-flex" id="archive-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1" >
 
 		<div class="row">
 
@@ -29,32 +29,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<header class="page-header">
 						<?php
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
 					</header><!-- .page-header -->
-					<div class="container row d-inline-flex justify-content-between"></div>
 						<?php /* Start the Loop */ ?>
+						<div class="container d-flex justify-content-around">
 						<?php while ( have_posts() ) : the_post(); ?>
 
-
 							<div class="col-3"> <?php get_template_part( 'loop-templates/content', 'dog-card' );?> </div>
-
-					</div>
 					<?php endwhile; ?>
-
+						</div>
 				<?php else : ?>
 
 				<?php endif; ?>
 
 			</main><!-- #main -->
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
-
 		</div> <!-- .row -->
 
 	</div><!-- #content -->
-
+			<!-- The pagination component -->
+			<?php understrap_pagination(); ?>
 	</div><!-- #archive-wrapper -->
 
 <?php get_footer(); ?>
