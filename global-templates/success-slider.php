6de3c284d96d167e497b3a7021d96fbc3b672c94
Fabian Olsson <fabtasticdesign@live.se>
2019-05-05 10:51:49 +0200

    rating function for success stories added

--- global-templates/success-slider.php
+++ global-templates/success-slider.php
@@ -5,6 +5,25 @@
             <li class="slide">
             <h1><?php the_title(); ?></h1>
 						<?php the_excerpt(); ?>
+
+
+
+						<?php
+						$rating = get_field( "rating" );
+						if($rating){
+						$star='<i class="fa fa-star-o fa-3x"></i>';
+						$score = '<i class="fa fa-star fa-3x"></i>';
+						$stars = array($star,$star,$star,$star,$star);
+
+						foreach (range(1, $rating) as $number) {
+							array_unshift ($stars, $score);
+							array_pop ($stars);
+						}
+							echo implode($stars,' ');
+						} ?>
+
+
+
           </li>
         <?php endwhile; endif; wp_reset_query(); ?>
         </ul>
