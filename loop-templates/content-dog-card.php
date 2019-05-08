6d6b2c5d64cc88ef289f43f728d80b03cf8424bd
Fabian Olsson <fabtasticdesign@live.se>
2019-05-08 16:25:19 +0200

    added adopted watermark

--- loop-templates/content-dog-card.php
+++ loop-templates/content-dog-card.php
@@ -1,7 +1,15 @@
 <div class="card info-card" style="width: 18rem;">
+	<div class="img-wrapper">
 		<?php the_post_thumbnail('weird'); ?>
-		<div class="card-body">
-			<h5 class="card-title"><?php the_title(); ?> </h5>
-			<p class="card-text"> <?php the_excerpt(); ?></p>
-		</div>
+		<?php 
+			if(has_term( 'dogs', 'sos_adopted')){
+				echo '<img class="img-adopted" src="' . get_stylesheet_directory_uri() . '/img/adopted.png" alt="adopted" />';
+			}
+		?>
+	</div>
+
+	<div class="card-body">
+		<h5 class="card-title"><?php the_title(); ?> </h5>
+		<p class="card-text"> <?php the_excerpt(); ?></p>
+	</div>
 </div>
